20e6db0b923ba4a1c8f577ee74f77aa15f4956f9
herifanantenana <herifanantenana17>
2026-05-10 10:00:41 +0300

    feat: integrate Clerk for authentication and add pricing table component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
@@ -15,7 +16,8 @@ const outfit = Outfit({
 
 export const metadata: Metadata = {
 	title: "Meetsy",
-	description: "Meetsy is a AI platform for connecting people with similar interests.",
+	description:
+		"Meetsy is a AI platform for connecting people with similar interests.",
 };
 
 export default function RootLayout({
@@ -28,7 +30,24 @@ export default function RootLayout({
 			lang="en"
 			className={cn("h-full antialiased", inter.variable, outfit.variable)}
 		>
-			<body className="min-h-full flex flex-col">{children}</body>
+			<ClerkProvider>
+				<body className="min-h-full flex flex-col">
+					<header className="flex justify-end items-center p-4 gap-4 h-16">
+						<Show when="signed-out">
+							<SignInButton />
+							<SignUpButton>
+								<button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+									Sign Up
+								</button>
+							</SignUpButton>
+						</Show>
+						<Show when="signed-in">
+							<UserButton />
+						</Show>
+					</header>
+					{children}
+				</body>
+			</ClerkProvider>
 		</html>
 	);
 }
